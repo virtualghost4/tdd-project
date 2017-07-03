f4ede9b9bc3580e8c383acf41cf02864b0e66f0d
virtualghost4 <manarias@alumnos.ubiobio.cl>
2017-07-03 19:17:21 -0400

    TipoConvenioController@index working

--- database/migrations/2017_06_30_170657_convenios.php
+++ database/migrations/2017_06_30_170657_convenios.php
@@ -18,7 +18,16 @@ class Convenios extends Migration
             $table->date('date_end');
             $table->string('vigencia');
             $table->string('objetivos');
+            $table->string('area_id')->unsigned();
+            $table->string('coordinador_id')->unsigned();
+            $table->string('tipo_convenio_id')->unsigned();
             $table->string('descripcion');
+
+            $table->foreign('area_id')->references('id')->on('area');
+            $table->foreign('coordinador_id')->references('id')->on('coordinador');
+            $table->foreign('tipo_convenio_id')->references('id')->on('tipo_convenio');
+
+
             $table->timestamps();
         });
     }
---
+++ database/migrations/2017_06_30_173242_actividades.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Actividades extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actividades', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('actividades');
+    }
+}
