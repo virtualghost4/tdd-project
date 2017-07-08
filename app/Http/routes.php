91b125963e91ee1082d597cc6c0e81d8afc202b5
Christopher66 <lazogame66@gmail.com>
2017-07-07 22:25:50 -0400

    Update routes.php
    Agregando ruta de documento

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,38 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
+
+Route::resource('convenios', 'ConveniosController');
+
+Route::resource('area', 'AreaController');
+
+Route::resource('documentos', 'DocumentosController');
+
+Route::group(['conv'=>'convenios'],function(){
+
+	Route::get('/convenios','ConveniosController@index');
+	Route::get('create','ConveniosController@create');
+	Route::get('eliminar','ConveniosController@destroy');
+	Route::post('store',['as'=>'store','uses'=>'ConveniosController@store']);
+});
+
+Route::group(['ar'=>'area'],function(){
+
+	Route::get('/area','AreaController@index');
+	Route::get('create','AreaController@create');
+	Route::get('eliminar','AreaController@destroy');
+	Route::post('store',['as'=>'store','uses'=>'AreaController@store']);
+});
+
+Route::group(['doc'=>'documentos'],function(){
+
+	Route::get('/documento','DocumentosController@index');
+	Route::get('create','DocumentosController@create');
+	Route::get('eliminar','DocumentosController@destroy');
+	Route::post('store',['as'=>'store','uses'=>'DocumentosController@store']);
+});
+
